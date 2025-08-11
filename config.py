67c7a54018526dc9ed893f82a4582f922feb1d6f
Gabriel Lobato Tavares <117449204+Gabriel4002@users.noreply.github.com>
2025-08-11 16:18:36 -0300

    Arquivo de config

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configurações do sistema
+COLUNAS_NOTA = ['Matematica', 'Portugues', 'Historia', 'Geografia']
+MEDIA_APROVACAO = 5.0
+DIRETORIO_SAIDA = 'analises'
+DIRETORIO_LOGS = 'logs'
+DIRETORIO_CACHE = 'cache'
+# Intervalo para recuperação (personalizável)
+RECUPERACAO_MIN = 0.0
+RECUPERACAO_MAX = 4.0
